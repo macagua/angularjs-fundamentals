1134fc8afbf7f9e6271ef0f9e7177e3ac0f2da84
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-06-22 10:11:41 -0400

    Realizado practica de modulo 3

--- pages/3-modules/app/app-final.js
+++ pages/3-modules/app/app-final.js
@@ -3,9 +3,15 @@
 
     var app = angular.module('myApp', []);
 
-    app.controller("myCtrl", function($scope) {
-        $scope.firstName = "Epilef";
-        $scope.lastName = "Rodriguez";
-    });
+    app.controller("myCtrl", ['$scope', function($scope) {
+        $scope.firstName = "Leonardo J.";
+        $scope.lastName = "Caballero G.";
+        $scope.x = null;
+        $scope.y = null;
+        $scope.sum = function (){
+            console.log('entro');
+            return $scope.x + $scope.y;
+        };
+    }]);
 
 })();
